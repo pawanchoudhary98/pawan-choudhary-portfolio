41ad4c684340442b10cacbe81c0cf66983c83191
pawanchoudhary98 <71929919+pawanchoudhary98@users.noreply.github.com>
2021-09-29 22:01:58 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 
+        var pre = document.getElementById("loading");
+
+        function preload() {
+            pre.style.display = "none";
+        }
+
+        $('.skill-per').each(function() {
+            var $this = $(this);
+            var per = $this.attr('per');
+            $this.css("width", per + '%');
+            $({
+                animatedValue: 0
+            }).animate({
+                animatedValue: per
+            }, {
+                duration: 1000,
+                step: function() {
+                    $this.attr('per', Math.floor(this.animatedValue) + '%');
+                },
+                complete: function() {
+                    $this.attr('per', Math.floor(this.animatedValue) + '%');
+                }
+            });
+        });
+    
+    
+
+        var typed = new Typed('.typing', {
+            strings: ["student.", "developer.", "designer."],
+            loop: true,
+            typeSpeed: 100,
+            backspeed: 1000,
+            backdelay: 1000
+        });
+   
+
+
+
+    
+    
+        AOS.init({
+            offset: 100,
+            duration: 2000
+
+        });
+   
